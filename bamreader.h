bedcd33fe2ea7103e46b4a51cb440995c92cd34d
ryan <ryan@ip-10-109-4-116.eu-west-1.compute.internal>
2022-11-30 02:51:08 +0000

    add support for reading BAMs directly from s3 buckets

--- bamreader.h
+++ bamreader.h
@@ -23,108 +23,112 @@
 #include "vector"
 
 // Samtools header files
-#include "bam.h"
-#include "sam.h"
-#include "kstring.h"
-#include "kseq.h"
-#include "khash.h"
-#include "ksort.h"
+#include "htslib/sam.h"
+#include "htslib/kstring.h"
+#include "htslib/kseq.h"
+#include "htslib/khash.h"
+#include "htslib/ksort.h"
 
 const unsigned g_SpacerBeforeAfter = 10000000;
 
 struct flags_hit {
-    flags_hit() {
-        mapped = false;
-        unique = false;
-        sw = false;
-        edits = 0;
-        suboptimal = false;
-    }
-    bool mapped;
-    bool unique;
-    bool sw;
-    int edits;
-    bool suboptimal;
+	flags_hit() {
+		mapped = false;
+		unique = false;
+		sw = false;
+		edits = 0;
+		suboptimal = false;
+	}
+	bool mapped;
+	bool unique;
+	bool sw;
+	int edits;
+	bool suboptimal;
 };
 
 /* initial from kai
-// struct of grab reads
-struct SPLIT_READ
-{
-    SPLIT_READ() 
-    {        
-        FragName = "";
-        Name = "";
-	Mapped = true;
-        ReadSeq = "";
-        MatchedD = 0;
-        MatchedRelPos = 0;
-        MS = 0;
-        Tag = "";
-    }
+ // struct of grab reads
+ struct SPLIT_READ
+ {
+ SPLIT_READ()
+ {
+ FragName = "";
+ Name = "";
+ Mapped = true;
+ ReadSeq = "";
+ MatchedD = 0;
+ MatchedRelPos = 0;
+ MS = 0;
+ Tag = "";
+ }
 
-    std::string FragName;
-    std::string Name;
-    bool Mapped;
-    char MatchedD; // rename AnchorStrand?
-    unsigned int MatchedRelPos;
-    short MS; // rename MappingQuality ?
-    std::string Tag; // rename SampleName ?
-    friend std::ostream& operator<<(std::ostream& os, const SPLIT_READ& splitRead);
-    std::string ReadSeq;
-};
-*/
+ std::string FragName;
+ std::string Name;
+ bool Mapped;
+ char MatchedD; // rename AnchorStrand?
+ unsigned int MatchedRelPos;
+ short MS; // rename MappingQuality ?
+ std::string Tag; // rename SampleName ?
+ friend std::ostream& operator<<(std::ostream& os, const SPLIT_READ& splitRead);
+ std::string ReadSeq;
+ };
+ */
 
 struct SPLIT_READ {
-    SPLIT_READ() {        
-        ReadSeq = "";
-        Mapped = true;
-        MatchedRelPos = 0;
-        
-    }
-    std::string ReadSeq;
-    bool Mapped;
-    unsigned int MatchedRelPos;
+	SPLIT_READ() {
+		ReadSeq = "";
+		Mapped = true;
+		MatchedRelPos = 0;
+
+	}
+	std::string ReadSeq;
+	bool Mapped;
+	unsigned int MatchedRelPos;
 };
 
 struct SupportPerSample {
-    int NumPlus;
-    int NumMinus;
-    int NumUPlus;
-    int NumUMinus;
-    SupportPerSample() { 
-        NumPlus = 0; 
-        NumMinus = 0; 
-        NumUPlus = 0; 
-        NumUMinus = 0; 
-    }
+	int NumPlus;
+	int NumMinus;
+	int NumUPlus;
+	int NumUMinus;
+	SupportPerSample() {
+		NumPlus = 0;
+		NumMinus = 0;
+		NumUPlus = 0;
+		NumUMinus = 0;
+	}
 };
 
 // read processing
 struct HomoSiteforBam {
-    HomoSiteforBam(){
-        length = 0;
-        type = '\0';
-        front_kmer = "";
-        end_kmer = "";
-    }
-    unsigned int length;
-    std::string front_kmer;
-    std::string end_kmer;
-    char type;
-    std::vector< unsigned int > distribution;
+	HomoSiteforBam() {
+		length = 0;
+		type = '\0';
+		front_kmer = "";
+		end_kmer = "";
+	}
+	unsigned int length;
+	std::string front_kmer;
+	std::string end_kmer;
+	char type;
+	std::vector<unsigned int> distribution;
 };
 
-void build_record( const bam1_t * mapped_read, 
-                   void *data, 
-                   const flags_hit *flag_current_read);
+void build_record(const bam1_t * mapped_read, void *data,
+		const flags_hit *flag_current_read);
 
-bool ReadInBamReads( const char *bam_path, 
-                     const std::string & FragName, 
-                     unsigned start, 
-                     unsigned end, 
-                     std::vector < SPLIT_READ > & AllReads, 
-                     std::string Tag);
+bool ReadInBamReads(const char *bam_path, const std::string & FragName,
+		unsigned start, unsigned end, std::vector<SPLIT_READ> & AllReads,
+		std::string Tag);
 
+
+//支持读取cram文件，需要额外参数 const char*ref_path
+bool ReadInCramReads(const char *bam_path, const char*ref_path,const std::string & FragName,
+                    unsigned start, unsigned end, std::vector<SPLIT_READ> & AllReads,
+                    std::string Tag);
+
+
+typedef int (*bam_fetch_f)(const bam1_t *b, void *data);
+int sam_fetch(samFile *fp, const hts_idx_t *idx, int tid, int beg, int end, void *data, bam_fetch_f func);
 #endif /* READER_H */
 
--- makefile
+++ makefile
@@ -1,26 +1,30 @@
 CXX=g++
-SAMTOOLS_ROOT=vendor/samtools-0.1.19
-
-
+HTSLIB=$(realpath vendor/htslib-1.16)
+CPP_ROOT=cpp
 CXXFLAGS+=    -O2 -fopenmp
-LDFLAGS+=    -L${SAMTOOLS_ROOT}
-LIBS+=    -lm -lbam -lz -lpthread
-INCLUDES+=    -I${SAMTOOLS_ROOT}
+LDFLAGS+=    -L${HTSLIB}
+LIBS+=    -lm -lz -lpthread -lhts
+INCLUDES+=    -I${HTSLIB}
+LD_LIBRARY_PATH+= -L${HTSLIB}
+export LD_LIBRARY_PATH=${HTSLIB_ROOT}
+export HTSLIB_CONFIGURE_OPTIONS="--enable-plugins,--enable-libcurl"
+
+
 SOURCE = cmds scan distribution refseq polyscan param utilities homo window bamreader sample chi somatic
 OBJS= $(patsubst %,%.o,$(SOURCE))
 
 %.o:%.cpp
-	        $(CXX) $(CXXFLAGS) $(INCLUDES) -c $< -o $@
+	        $(CXX) -L ${HTSLIB} $(CXXFLAGS) $(INCLUDES) -c $< -o $@
 
-all: samtools msisensor
+all: htsfile msisensor
 
-samtools:
-	        $(MAKE) -C ${SAMTOOLS_ROOT}
+htsfile:
+	        $(MAKE) -C ${HTSLIB}
 
 msisensor: $(OBJS)
-	        $(CXX) $^ $(CXXFLAGS) $(LDFLAGS) $(LIBS) -o $@ 
+	       $(CXX) $^ $(CXXFLAGS) $(LDFLAGS) $(LIBS) -Wl,-rpath=${HTSLIB} -o $@
 
 clean:
 	        rm -f *.o msisensor
-			        $(MAKE) -C ${SAMTOOLS_ROOT} clean
+			$(MAKE) -C ${HTSLIB} clean
 
